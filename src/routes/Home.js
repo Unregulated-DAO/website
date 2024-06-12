27e9bf8b5864b41ba5533f7c088a0e27bc094f0d
0xfatman <0xfatman@proton.me>
2024-06-12 20:11:57 +0100

    fix mobile view

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,17 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <>
-    {/*<>
-      <h1>TAKI &#x1F977;</h1>
-      <h2>[<span class="grey">stealth.mode = enabled</span>]</h2>
-    </>*/}
-      <div>
-        <img src="nature.png" width="64" />
-        <h1>Taki</h1>
-        <h2>A different kind of DEX</h2>
-      </div>
-    </>
+    <div className="vega-content">
+      <img src="nature.png" width="64" />
+      <h1>Taki</h1>
+      <h2>A different kind of DEX</h2>
+    </div>
   )
 }
 
