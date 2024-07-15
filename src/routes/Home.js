e9ffe32c00bc53966b0885d81a0a1361c4c23226
0xfatman <0xfatman@proton.me>
2024-07-15 18:37:06 +0100

    updated home page

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,9 +3,11 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="vega-content">
-      <img src="nature.png" width="64" />
-      <h1>Taki</h1>
+      <div className="cover">
+        <img src="TakiLogoWhite.svg"></img>
+      </div>
       <h2>A different kind of DEX</h2>
+      <p>Fixing fragmented liquidity in DeFi by offering leverage trading on thousands of real-world assets on-chain. Currently in stealth mode.</p>
     </div>
   )
 }
