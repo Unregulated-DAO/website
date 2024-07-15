9fbbdb44eb8213ed6f35b7a65515e077ce4465a8
0xfatman <0xfatman@proton.me>
2024-07-15 18:48:28 +0100

    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
         <img src="TakiLogoWhite.svg"></img>
       </div>
       <h2>A different kind of DEX</h2>
-      <p>Fixing fragmented liquidity in DeFi by offering leverage trading on thousands of real-world assets on-chain. Currently in stealth mode.</p>
+      <p>Fixing fragmented liquidity in DeFi by offering leverage trading on thousands of real-world assets. Currently in stealth mode.</p>
     </div>
   )
 }
